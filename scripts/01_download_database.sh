82e11ba52448c0a9e4ad6506b91722552a5b0f80
farah-y-sadoon <fsadoon@uoguelph.ca>
2026-03-17 21:47:16 -0400

    Change download database script to get kraken2 standard db

--- scripts/01_download_database.sh
+++ scripts/01_download_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to download KrakenUniq standard database
+# Script to download Kraken2 standard database
 
 #SBATCH --job-name=download_db
 #SBATCH --time=08:00:00
@@ -8,26 +8,24 @@
 #SBATCH --mem=8G
 #SBATCH --output=logs/download_db_%j.txt
 
-# Define directory paths
-KRAKEN_UNIQ_DB="../krakenuniq_db"
+# Define directory path
+KRAKEN2_DB="../kraken2_db"
 
-# Make krakenuniq_db directory and change directories
-mkdir -p "$KRAKEN_UNIQ_DB"
-cd "$KRAKEN_UNIQ_DB"
+# Make kraken2_db directory and change directories
+mkdir -p "$KRAKEN2_DB"
+cd "$KRAKEN2_DB"
 
 # Download database
-echo "Downloading the database.kdb..."
-wget https://genome-idx.s3.amazonaws.com/kraken/uniq/krakendb-2022-06-16-STANDARD/database.kdb
-
-echo "Downloading the archive..."
-wget https://genome-idx.s3.amazonaws.com/kraken/uniq/krakendb-2022-06-16-STANDARD/kuniq_standard_minus_kdb.20220616.tgz
+echo "Downloading the database..."
+wget https://genome-idx.s3.amazonaws.com/kraken/k2_standard_20251015.tar.gz
 
 # Extract the archive
-echo "Extracting archive..."
-tar -xzf kuniq_standard_minus_kdb.20220616.tgz
+echo "Extracting ..."
+tar -xzf k2_standard_20251015.tar.gz
+rm k2_standard_20251015.tar.gz
 
 echo "Download and extraction complete."
 
 # Print database directory contents
 echo "Verifying database contents:"
-ls -lh "$KRAKEN_UNIQ_DB"
+ls -lh "$KRAKEN2_DB"
